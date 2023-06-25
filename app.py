a9cb98a6232552f1eb0c357f1742ab9f56a93dbc
Hiacecommuter <82562175+Hiacecommuter@users.noreply.github.com>
2023-06-25 22:56:00 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask
+from datetime import date
 
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def home():
-    return "<div><p>welcome to the test home</p></div>"
+    Oly_date = date.fromisoformat("2032-07-23")
+    today = date.today()
+    return "<div><p>f"Countdown to Brisbane Olympic 2032: {Oly_date - today}"</p></div>"
 
 
 if __name__ == "__main__":
