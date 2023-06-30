daf2ac64e5e21993979f2cd389f17e59ca3873f7
Hiacecommuter <guo55wei@gmail.com>
2023-06-30 12:02:46 +1000

    initial commit

--- app.py
+++ app.py
@@ -1,15 +1,34 @@
-from flask import Flask
-from datetime import date
+from flask import Flask, render_template, flash, redirect, url_for
+from datetime import datetime
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    Oly_date = date.fromisoformat("2032-07-23")
-    today = date.today()
-    return "<div><p>f"Countdown to Brisbane Olympic 2032: {Oly_date - today}"</p></div>"
+    return render_template("index.html")
+
+@app.route('/countdown/')
+def countdown():
+    olympics = datetime(2032, 7, 23, 10, 0, 0).timestamp()
+    now = datetime.now().timestamp()
+    diff_sec = round(olympics - now)
+    return render_template("countdown.html", total_sec=diff_sec)
+
+@app.route('/sudoku/')
+def sudoku():
+    return render_template("sudoku.html")
+
+@app.route("/stopwatch/")
+def stopwatch():
+    return render_template("stopwatch.html")
+
+@app.errorhandler(404)
+def page_not_found(e):
+    flash("Something goes wrong ... \n That page cannot be found")
+    return redirect(url_for("home"))
 
 
 if __name__ == "__main__":
-    app.run()
+    app.secret_key = "This is a long sec key"
+    app.run(debug=True)
